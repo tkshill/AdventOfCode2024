42c32c873719ca7bddb77f8f6ac441f123244b65
Kirk <tkshillinz@icloud.com>
2024-12-03 22:13:45 -0700

    broke it then fixed it

--- AOC/Day02.fs
+++ AOC/Day02.fs
@@ -8,7 +8,7 @@ let isSafe lst =
     let diff = uncurry (-)
     let safeItem f value = f (diff value) && abs (diff value) > 0 && abs (diff value) < 4
 
-    forall (safeItem ((>) 0)) lst && forall (safeItem ((<) 0)) lst
+    forall (safeItem ((>) 0)) lst || forall (safeItem ((<) 0)) lst
 
 let solve safeCheck =
     let lineParser = %% +.(p<int> * (qty[1..] / ' ')) -- spaces -|> ResizeArray.toSeq
@@ -22,11 +22,8 @@ let part1 input =
 
 let isSafe2 (lst: list<int * int>)  =
     seq { 
-        yield lst 
-        yield lst[1..]
-        yield lst[0..(length lst - 2)]
-        for i in 0..(length lst - 2) do
-            yield lst[0..i - 1] @ [fst lst[i], snd lst[i + 1]] @ lst[i + 2..]            
+        yield lst; yield lst[1..]; yield lst[0..(length lst - 2)]
+        for i in 0..(length lst - 2) do yield lst[0..i - 1] @ [fst lst[i], snd lst[i + 1]] @ lst[i + 2..];            
     } |> exists isSafe 
 
 let part2 input =
