5acc40722db2ddaaca11a3193682841c8b709abd
Kirk <tkshillinz@icloud.com>
2024-12-03 22:10:15 -0700

    more readable safe check

--- AOC/Day02.fs
+++ AOC/Day02.fs
@@ -7,10 +7,8 @@ open FParsec.Pipes
 let isSafe lst =
     let diff = uncurry (-)
     let safeItem f value = f (diff value) && abs (diff value) > 0 && abs (diff value) < 4
-  
-    [(>) 0; (<) 0]
-    |> map (safeItem >> flip forall lst)
-    |> reduce (||)
+
+    forall (safeItem ((>) 0)) lst && forall (safeItem ((<) 0)) lst
 
 let solve safeCheck =
     let lineParser = %% +.(p<int> * (qty[1..] / ' ')) -- spaces -|> ResizeArray.toSeq
@@ -23,13 +21,13 @@ let part1 input =
     solve isSafe input
 
 let isSafe2 (lst: list<int * int>)  =
-        seq { 
-            yield lst 
-            yield lst[1..]
-            yield lst[0..(length lst - 2)]
-            for i in 0..(length lst - 2) do
-                yield lst[0..i - 1] @ [fst lst[i], snd lst[i + 1]] @ lst[i + 2..]            
-        } |> exists isSafe 
+    seq { 
+        yield lst 
+        yield lst[1..]
+        yield lst[0..(length lst - 2)]
+        for i in 0..(length lst - 2) do
+            yield lst[0..i - 1] @ [fst lst[i], snd lst[i + 1]] @ lst[i + 2..]            
+    } |> exists isSafe 
 
 let part2 input =
     solve isSafe2 input
