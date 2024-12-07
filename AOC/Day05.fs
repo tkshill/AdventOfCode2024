f3188b1990d7a2d055afecfd62464463af0d8a5d
Kirk <tkshillinz@icloud.com>
2024-12-06 22:55:37 -0700

    edits

--- AOC/Day05.fs
+++ AOC/Day05.fs
@@ -3,42 +3,42 @@ module Day05
 open FParsec
 open FParsec.Pipes
 
-let folder (dict: Map<int, int Set>) (k, v) =
-    let newK = dict.TryFind k |> withDefault Set.empty |> Set.add v
-    let newv = dict.TryFind v |> withDefault Set.empty
+let foldPairIntoMap (dict: Map<int, int Set>) (biggerPage, smallerPage) =
+    let valuesForBigger = dict.TryFind biggerPage |> withDefault Set.empty |> Set.add smallerPage
+    let valuesForSmaller = dict.TryFind smallerPage |> withDefault Set.empty
 
-    dict.Add(k, newK).Add(v, newv)
+    dict.Add(biggerPage, valuesForBigger).Add(smallerPage, valuesForSmaller)
 
 let parse = 
     (sepEndBy1 (pint32 .>> %'|' .>>. pint32) %'\n') .>> %'\n' .>>. sepBy1 (sepBy1 pint32 %',') %'\n' 
-    |>> mapT (foldl folder Map.empty) id
+    |>> mapT (foldl foldPairIntoMap Map.empty) id
 
 let rec isSorted (dict: Map<int, int Set>) = function
     | _ :: [] -> true
-    | x :: xs when Set.isSubset (Set xs) dict[x] |> not -> false
-    | _ :: xs -> isSorted dict xs
+    | page :: others when Set.isSubset (Set others) dict[page] |> not -> false
+    | _ :: others -> isSorted dict others
 
 let middle items = item (length items / 2) items  // items[items.Length / 2]
 
 let part1 input = 
-    let dict, pages = runParser parse input
+    let dict, pagesList = runParser parse input
     
-    pages |> filter (isSorted dict) |> sumBy middle
+    pagesList |> filter (isSorted dict) |> sumBy middle
 
-let refine (dict: Map<int, int Set>) items  =
-    Map.filter (curry (fst >> eq >> flip exists items)) dict
-    |> Map.map (curry (snd >> Set.filter (eq >> flip exists items)))
+let refine (dict: Map<int, int Set>) pages  =
+    Map.filter (curry (fst >> eq >> flip exists pages)) dict
+    |> Map.map (curry (snd >> Set.filter (eq >> flip exists pages)))
 
-let comparer (dict: Map<int, int Set>) = 
+let emitInOrder (dict: Map<int, int Set>) = 
     match toList dict.Keys with
     | [] -> None
-    | items ->
-        let allDeps = Map.fold (fun acc _ v -> Set.union acc v) Set.empty dict 
-        let highest :: _ , rest = List.partition (flip Set.contains allDeps >> not) items 
+    | pageNumbers ->
+        let allSubPages = Map.fold (fun acc _ v -> Set.union acc v) Set.empty dict 
+        let highest :: _ , rest = List.partition (flip Set.contains allSubPages >> not) pageNumbers 
 
         Some (highest, (refine dict rest))
 
 let part2 input =
-    let dict, pages = runParser parse input
+    let dict, pagesList = runParser parse input
     
-    filter (isSorted dict >> not) pages |> sumBy (refine dict >> unfold comparer >> toList >> middle) 
+    filter (isSorted dict >> not) pagesList |> sumBy (refine dict >> unfold emitInOrder >> toList >> middle) 
