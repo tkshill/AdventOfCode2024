1bb01090d6867b14719d77a7f1f6b5bcf0e6c5c8
Kirk <tkshillinz@icloud.com>
2024-12-08 01:32:37 -0700

    tweaks

--- AOC/Day06.fs
+++ AOC/Day06.fs
@@ -8,9 +8,8 @@ let parse input =
     |> partition (fst >> lessThan 0)
     |> fun (guards, obstructions) -> head guards |> spread (mul -1), Set obstructions, (maxX, maxY)
 
-let rotate (x, y) = y, x * -1
-let moveGuard (moveX, moveY) (guardX, guardY) = guardX + moveX, guardY + moveY
-let stillIn xMax yMax (x, y) = not (x < 0 || y < 0 || x > xMax || y > yMax)
+let rotate aim = snd aim, fst aim * -1
+let moveGuard translation guard = fst guard + fst translation, snd guard + snd translation
 
 let patrolRoute obstructions (aim, guard) =
     let potentialGuard = moveGuard aim guard
@@ -22,7 +21,9 @@ let patrolRoute obstructions (aim, guard) =
         Some (nextGuard, (nextTranslation, nextGuard))
     else Some (potentialGuard, (aim, potentialGuard))
 
-let guardPath router seed obstructions xMax yMax = unfold (router  obstructions) seed |> takeWhile (stillIn xMax yMax)
+let guardPath router seed obstructions xMax yMax = 
+    let stillIn (x, y) = not (x < 0 || y < 0 || x > xMax || y > yMax)
+    unfold (router  obstructions) seed |> takeWhile stillIn 
 
 let part1 input =
     let guard, obstructions, (xMax, yMax) = parse input
@@ -36,21 +37,21 @@ let sameGradient (deltaX, deltaY) (x, y) (x1, y1) =
     elif float deltaY = 0.0 && float y = float y1 then ((float x1 - float x) / (abs (float x1 - float x))) = float deltaX
     else false
 
-let closest (x, y: int) (x1, y1) = (abs (x - x1)) + (abs (y - y1))
-let rec patrolRoute2 obstructions (collisions, aim, guard) =
+let rec willLoop obstructions (collisions, aim, guard) =
+    let distance (x, y: int) (x1, y1) = (abs (x - x1)) + (abs (y - y1))
     let incomingObstructions = Set.filter (sameGradient aim guard) obstructions
 
     if isEmpty incomingObstructions then 0
     else
-        let closestObstruction = minBy (closest guard) incomingObstructions
+        let closestObstruction = minBy (distance guard) incomingObstructions
         let beforeCollision = moveGuard (spread (mul -1) aim) closestObstruction
         let collision = closestObstruction, beforeCollision
 
         if contains collision collisions then 1
-        else patrolRoute2 obstructions (collision :: collisions, rotate aim, beforeCollision)
+        else willLoop obstructions (collision :: collisions, rotate aim, beforeCollision)
 
 let part2 input =
     let guard, obstructions, (xMax, yMax) = parse input
 
     guardPath patrolRoute ((-1, 0), guard) obstructions xMax yMax |> Set |> Set.remove guard
-    |> sumBy (flip Set.add obstructions >> flip patrolRoute2 ([], (-1, 0), guard)) 
+    |> sumBy (flip Set.add obstructions >> flip willLoop ([], (-1, 0), guard)) 
