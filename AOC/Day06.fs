38597596df929195f33f224f1677eccb8bb53640
Kirk <tkshillinz@icloud.com>
2024-12-08 01:52:29 -0700

    final naming changes

--- AOC/Day06.fs
+++ AOC/Day06.fs
@@ -8,8 +8,8 @@ let parse input =
     |> partition (fst >> lessThan 0)
     |> fun (guards, obstructions) -> head guards |> spread (mul -1), Set obstructions, (maxX, maxY)
 
-let rotate aim = snd aim, fst aim * -1
-let moveGuard translation guard = fst guard + fst translation, snd guard + snd translation
+let rotate (deltaX, deltaY) = deltaY, deltaX * -1
+let moveGuard (deltaX, deltaY) (guardX, guardY) = guardX + deltaX, guardY + deltaY
 
 let patrolRoute obstructions (aim, guard) =
     let potentialGuard = moveGuard aim guard
@@ -21,25 +21,26 @@ let patrolRoute obstructions (aim, guard) =
         Some (nextGuard, (nextTranslation, nextGuard))
     else Some (potentialGuard, (aim, potentialGuard))
 
-let guardPath router seed obstructions xMax yMax = 
+let generatePath router seed obstructions xMax yMax = 
     let stillIn (x, y) = not (x < 0 || y < 0 || x > xMax || y > yMax)
-    unfold (router  obstructions) seed |> takeWhile stillIn 
+    
+    unfold (router obstructions) seed |> takeWhile stillIn 
 
 let part1 input =
     let guard, obstructions, (xMax, yMax) = parse input
 
-    guardPath patrolRoute ((-1, 0), guard) obstructions xMax yMax |> Set |> Set.add guard |> length
+    generatePath patrolRoute ((-1, 0), guard) obstructions xMax yMax |> Set |> Set.add guard |> length
 
-let sameGradient (deltaX, deltaY) (x, y) (x1, y1) = 
+let isOncoming (deltaX, deltaY) (guardX, guardY) (obstacleX, obstacleY) = 
 
-    if float deltaX = 0.0 && float x = float x1
-    then ((float y1 - float y) / (abs (float y1 - float y))) = float deltaY
-    elif float deltaY = 0.0 && float y = float y1 then ((float x1 - float x) / (abs (float x1 - float x))) = float deltaX
+    if float deltaX = 0.0 && float guardX = float obstacleX
+    then ((float obstacleY - float guardY) / (abs (float obstacleY - float guardY))) = float deltaY
+    elif float deltaY = 0.0 && float guardY = float obstacleY then ((float obstacleX - float guardX) / (abs (float obstacleX - float guardX))) = float deltaX
     else false
 
 let rec willLoop obstructions (collisions, aim, guard) =
     let distance (x, y: int) (x1, y1) = (abs (x - x1)) + (abs (y - y1))
-    let incomingObstructions = Set.filter (sameGradient aim guard) obstructions
+    let incomingObstructions = Set.filter (isOncoming aim guard) obstructions
 
     if isEmpty incomingObstructions then 0
     else
@@ -53,5 +54,5 @@ let rec willLoop obstructions (collisions, aim, guard) =
 let part2 input =
     let guard, obstructions, (xMax, yMax) = parse input
 
-    guardPath patrolRoute ((-1, 0), guard) obstructions xMax yMax |> Set |> Set.remove guard
+    generatePath patrolRoute ((-1, 0), guard) obstructions xMax yMax |> Set |> Set.remove guard
     |> sumBy (flip Set.add obstructions >> flip willLoop ([], (-1, 0), guard)) 
