d77f84c08cae64ed3e5466aa15fa5d3ffab9267a
Kirk <tkshillinz@icloud.com>
2024-12-08 13:19:15 -0700

    spacing!

--- AOC/Day07.fs
+++ AOC/Day07.fs
@@ -7,7 +7,7 @@ let parseLine = (pint64 |>> bigint) .>> %": " .>>. sepEndBy1 (pint64 |>> bigint)
 
 let rec options ops n = 
     let subPerms perm = map (fun op -> seq { yield op; yield! perm }) ops
-    
+
     seq { if n = 0 then yield empty else for perm in options ops (n - 1) do yield! subPerms perm } 
     
 let solve ops (goal, head :: rest) = 
@@ -17,5 +17,6 @@ let part1 input =
     split "\n" input |> map (runParser parseLine) |> filter (solve [(*); (+)]) |> sumBy fst
 
 let combine i j = $"{j}{i}" |> int64 |> bigint
+
 let part2 input = 
     split "\n" input |> map (runParser parseLine) |> filter (solve [(*); (+); combine]) |> sumBy fst 
