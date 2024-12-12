802e324fe9b133dec0976766ce2931d371955273
Kirk <tkshillinz@icloud.com>
2024-12-12 06:51:32 -0700

    edits

--- AOC/Day11.fs
+++ AOC/Day11.fs
@@ -2,32 +2,31 @@ module Day11
 
 open FParsec
 open FParsec.Pipes
-open System
 
 let solomon = 
-    string >> splitInto 2 >> map (String >> uint64) >> toList
+    string >> splitInto 2 >> map (System.String >> uint64) >> toList
 
-let updateNum = function
+let blinkStone = function
     | 0UL -> [1UL]
     | x when string x |> length |> isEven -> solomon x
     | x -> [x * 2024UL]
 
-let updates (stone, count) = 
-    updateNum stone |> map (fun k -> k, count)
+let updates (stone, occurances) = 
+    blinkStone stone |> map (fun k -> k, occurances)
 
-let rec folder input  = function
-    | 0 -> sumBy snd input
-    | n ->
-        collect updates input 
+let rec folder stoneOccurences  = function
+    | 0 -> sumBy snd stoneOccurences
+    | blinksLeft ->
+        collect updates stoneOccurences 
         |> groupBy fst
         |> map (mapT id (sumBy snd))
-        |> flip folder (n - 1)
+        |> flip folder (blinksLeft - 1)
 
-let solve limit  =
+let solve totalBlinks  =
     runParser (sepBy1 puint64 %' ')
     >> Seq.countBy id
     >> map (mapT id uint64)
-    >> flip folder limit
+    >> flip folder totalBlinks
 
 let part1 input = solve 25 input
 
