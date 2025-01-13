3b33ec7fb3682aef9ff61873178b46279cc43c51
Kirk <tkshillinz@icloud.com>
2025-01-13 07:54:36 -0700

    no mas

--- AOC/Day13.fs
+++ AOC/Day13.fs
@@ -3,19 +3,20 @@ module Day13
 open FParsec
 open FParsec.Pipes
 
-let pLine = %["Button A: "; "Button B: "; "Prize: " ] >>. %["X+"; "X="] >>. %p<double> .>> %[", Y+"; ", Y="] .>>. %p<double>
+let inputParser = 
+    let lineParser = %["Button A: "; "Button B: "; "Prize: " ] >>. %["X+"; "X="] >>. %p<double> .>> %[", Y+"; ", Y="] .>>. %p<double>
 
-let pInput = sepEndBy1 pLine (many1 newline)
+    sepEndBy1 lineParser (many1 newline)
 
-let toEquations offset ((x1, y1) :: (x2, y2) :: (prizeX, prizeY) :: _) = (x1, x2, prizeX + offset), (y1, y2, prizeY + offset)
+let toSimultaneous offset ((x1, y1) :: (x2, y2) :: (prizeX, prizeY) :: _) = (x1, x2, prizeX + offset), (y1, y2, prizeY + offset)
 
 let solveSimulatenous ((a1, b1, c1), (a2, b2, c2)) = 
-    let ``presses of B`` = (a1 * c2 - a2 * c1) / (a1 * b2 - a2 * b1)
-    let ``presses of A`` = (c1 - b1 * ``presses of B``) / a1
+    let Bpresses = (a1 * c2 - a2 * c1) / (a1 * b2 - a2 * b1)
+    let Apresses = (c1 - b1 * Bpresses) / a1
 
-    if ``presses of A`` = floor ``presses of A`` && ``presses of B`` = floor ``presses of B`` then ``presses of A`` * 3.0 + ``presses of B`` else 0.0
+    if Apresses = floor Apresses && Bpresses = floor Bpresses then Apresses * 3.0 + Bpresses else 0.0
 
-let solve offset = runParser pInput >> chunkBySize 3 >> sumBy (toList >> toEquations offset >> solveSimulatenous)
+let solve offset = runParser inputParser >> chunkBySize 3 >> sumBy (toList >> toSimultaneous offset >> solveSimulatenous)
 
 let part1 input = solve 0 input
 
